abf660444dfeba03b916fc036f882ff3695dc285
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2024-06-18 10:49:14 +0800

    remove cgo

--- protocol/t125/mcs.go
+++ protocol/t125/mcs.go
@@ -8,7 +8,7 @@ import (
 	"io"
 	"reflect"
 
-	"github.com/tomatome/grdp/plugin/cliprdr"
+	// "github.com/tomatome/grdp/plugin/cliprdr"
 	"github.com/tomatome/grdp/plugin/rail"
 
 	"github.com/tomatome/grdp/plugin/drdynvc"
@@ -291,9 +291,9 @@ func (c *MCSClient) SetClientRemoteProgram() {
 	c.clientNetworkData.AddVirtualChannel(rail.ChannelName, rail.ChannelOption)
 }
 
-func (c *MCSClient) SetClientCliprdr() {
-	c.clientNetworkData.AddVirtualChannel(cliprdr.ChannelName, cliprdr.ChannelOption)
-}
+// func (c *MCSClient) SetClientCliprdr() {
+// 	c.clientNetworkData.AddVirtualChannel(cliprdr.ChannelName, cliprdr.ChannelOption)
+// }
 
 func (c *MCSClient) connect(selectedProtocol uint32) {
 	glog.Debug("mcs client on connect", selectedProtocol)
